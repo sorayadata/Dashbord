3fd054ed36b00c92561df528daa4e16c13267e82
KAHLOUCHE <kahlouchesoraya27@gmail.com>
2024-05-30 20:16:34 +0200

    dashboard pret etre deployé

--- dashbo.py
+++ dashbo.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # url_api = 'http://localhost:5000'  # local API
-url_api = "http://13.38.75.218:5000"  # online API
+url_api = "http://13.38.60.118:5000"  # online API
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
